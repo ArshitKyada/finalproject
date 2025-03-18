4af642fc4c470c4e77c08c45c3794687d7781cb7
Arshit5471 <arshitkyada75@gmail.com>
2025-03-18 13:15:35 +0530

    18/03 | 1:15

--- payment_page.php
+++ payment_page.php
@@ -1,3 +1,38 @@
+<?php
+// Start the session
+session_start();
+
+include_once 'connect.php';
+
+// Process form submission
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
+    $full_name = $_POST['full_name'];
+    $email = $_POST['email'];
+    $address = $_POST['address'];
+    $city = $_POST['city'];
+    $state = $_POST['state'];
+    $zip_code = $_POST['zip_code'];
+    $card_name = $_POST['card_name'];
+    $card_number = $_POST['card_number'];
+    $exp_month = $_POST['exp_month'];
+    $exp_year = $_POST['exp_year'];
+    $cvv = $_POST['cvv'];
+
+    // Basic SQL query to insert data
+    $sql = "INSERT INTO payments (full_name, email, address, city, state, zip_code, card_name, card_number, exp_month, exp_year, cvv) 
+            VALUES ('$full_name', '$email', '$address', '$city', '$state', '$zip_code', '$card_name', '$card_number', '$exp_month', '$exp_year', '$cvv')";
+
+    // Execute the query
+    if ($conn->query($sql) === TRUE) {
+        echo "<script>alert('Payment details stored successfully!'); window.location.href = 'thank_you.php';</script>";
+    } else {
+        echo "<script>alert('Error storing payment details: " . $conn->error . "');</script>";
+    }
+}
+
+// HTML Form
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,42 +40,42 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Responsive Payment Gateway Form Design using HTML and CSS | Codehal</title>
+    <title>Payment Gateway Form</title>
     <link rel="stylesheet" href="css/paymentstyle.css">
 </head>
 
 <body>
 
     <div class="container">
-        <form action="">
+        <form action="" method="POST">
             <div class="row">
                 <div class="column">
                     <h3 class="title">Billing Address</h3>
                     <div class="input-box">
                         <span>Full Name :</span>
-                        <input type="text" placeholder="Jacob Aiden">
+                        <input type="text" name="full_name" placeholder="Jacob Aiden" required>
                     </div>
                     <div class="input-box">
                         <span>Email :</span>
-                        <input type="email" placeholder="example@example.com">
+                        <input type="email" name="email" placeholder="example@example.com" required>
                     </div>
                     <div class="input-box">
                         <span>Address :</span>
-                        <input type="text" placeholder="Room - Street - Locality">
+                        <input type="text" name="address" placeholder="Room - Street - Locality" required>
                     </div>
                     <div class="input-box">
                         <span>City :</span>
-                        <input type="text" placeholder="Berlin">
+                        <input type="text" name="city" placeholder="Berlin" required>
                     </div>
 
                     <div class="flex">
                         <div class="input-box">
                             <span>State :</span>
-                            <input type="text" placeholder="Germany">
+                            <input type="text" name="state" placeholder="Germany" required>
                         </div>
                         <div class="input-box">
                             <span>Zip Code :</span>
-                            <input type="number" placeholder="123 456">
+                            <input type="number" name="zip_code" placeholder="123 456" required>
                         </div>
                     </div>
                 </div>
@@ -53,25 +88,25 @@
                     </div>
                     <div class="input-box">
                         <span>Name On Card :</span>
-                        <input type="text" placeholder="Mr. Jacob Aiden">
+                        <input type="text" name="card_name" placeholder="Mr. Jacob Aiden" required>
                     </div>
                     <div class="input-box">
                         <span>Credit Card Number :</span>
-                        <input type="number" placeholder="1111 2222 3333 4444">
+                        <input type="text" name="card_number" placeholder="1111 2222 3333 4444" required>
                     </div>
                     <div class="input-box">
                         <span>Exp. Month :</span>
-                        <input type="text" placeholder="August">
+                        <input type="text" name="exp_month " placeholder="August" required>
                     </div>
-                
+
                     <div class="flex">
                         <div class="input-box">
                             <span>Exp. Year :</span>
-                            <input type="number" placeholder="2025">
+                            <input type="number" name="exp_year" placeholder="2025" required>
                         </div>
                         <div class="input-box">
                             <span>CVV :</span>
-                            <input type="number" placeholder="123">
+                            <input type="number" name="cvv" placeholder="123" required>
                         </div>
                     </div>
                 </div>
